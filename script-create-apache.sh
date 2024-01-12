23ff9ea361193746a1f00eb0409511ace0dc1ca5
Henderson-dev <teconet.arte@gmail.com>
2024-01-12 19:48:44 -0300

    Update script

--- script-create-apache.sh
+++ script-create-apache.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Atualizando o Servidor"
+echo "----------------------"
+
 # Faz update e upgrade do servidor
 apt-get update
 apt-get upgrade -y
@@ -17,4 +20,14 @@ echo "--------------------------------------------"
 # posiciona na pasta temporária
 cd /tmp
 
-wget 
+# Baixar arquivos do site no repositorio do GitHub
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+
+# Descompacta os arquivos na pasta linux-site-dio
+unzip main.zip
+
+# Posiciona na pasta onde os arquivos foram descompactados
+cd linux-site-dio-main
+
+# Copia os arquivos da aplicação para pasta raiz do servidor apache
+cp -R * /var/www/html/
